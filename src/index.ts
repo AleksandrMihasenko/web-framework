4e6d17ca4402792244530e37683e49c859108d3c
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2022-05-22 17:52:40 +0300

    implement handlers, saving and launch them

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'Aleks', age: 20 });
-user.set({ name: 'Aleksandr', age: 30 });
-console.log(user.get('name'));
+user.on('change', () => {
+  console.log('change 1');
+});
+user.on('change', () => {
+  console.log('change 2');
+});
+user.on('click', () => {
+  console.log('click 1');
+});
+user.trigger('change');
+user.trigger('click');
