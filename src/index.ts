c450702b4873c01d1918465c97588534196e645c
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2022-07-03 14:46:08 +0300

    refactoring, add set method

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import { User } from './models/User';
 
 const user = new User({name: 'new record', age: 7});
+
+console.log(user.get('name'));
+
+user.on('change', () => {
+  console.log('change');
+})
+user.set({name: 'New name'});
