4ee56e3fda2d6a90b6968b6f80cdfbab2cae5840
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2022-08-11 18:47:11 +0300

    minor fixes

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,17 @@ import { UserForm } from './views/UserForm';
 import { User } from './models/User';
 
 const user = User.buildUser({ name: 'test', age: 20 });
-const userForm = new UserForm(
-  document.getElementById('app'),
-  user
-);
+const root = document.getElementById('app');
 
-userForm.render();
+if (root) {
+  const userForm = new UserForm(
+    document.getElementById('app'),
+    user
+  );
+  
+  userForm.render();
+} else {
+  throw new Error('Root element not found');
+}
+
+
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -20,11 +20,13 @@ export class UserForm {
   }
   
   onAddNameClick = (): void => {
-    console.log('click');
     const input = this.parent?.querySelector('input');
-    const name = input?.value;
     
-    this.model.set({ name });
+    if (input) {
+      const name = input.value;
+  
+      this.model.set({ name });
+    }
   }
   
   onAddAgeClick = (): void => {
